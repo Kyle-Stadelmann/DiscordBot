7cb1e22d34e52c3a2086f55a350b0dccea85f88b
Kyle Stadelmann <dlice@hotmail.com>
2021-08-09 18:03:40 -0700

    Watches/reloads more files, may need refactoring

--- util/reinitBot.js
+++ util/reinitBot.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { constants } = require('buffer');
 
 // Watches important bot files (e.g. commands, events, constants, etc.) to
 // register when files have been changed for use in reInitBot whenever a message is
@@ -7,18 +8,29 @@ const fs = require('fs');
 // Input: Bot client that contains the bot.changedFiles map that's used to register
 // when a relevant file has changed.
 module.exports.watchBotFiles = (bot) => {
-    bot.changedFiles = {};
-    bot.changedFiles.commands = new Map();
+    // Init the changedFiles property that tracks our files we want to watch for changes
+    bot.changedFiles = {
+        commands: new Map(),
+        util: new Map(),
+        eventLibs: {},
+        constants: false,
+        event_percentages: false
+    };
 
     const rootDir = __dirname + "/.."
 
-    // Watch command files
+    // Watch command, util directories
     const cmdDir = rootDir + "/commands/";
-    fs.watch(cmdDir, (event, filename) => {
-        if (filename && event === "change" && filterJsFiles(filename)) {
-            bot.changedFiles.commands.set(cmdDir + filename, true);
-        }
-    });
+    watchBotDir(cmdDir, bot.changedFiles.commands);
+    const utilDir = rootDir + "/util/";
+    watchBotDir(utilDir, bot.changedFiles.util);
+
+    // Watch constants, event_percentages files
+    const constFile = rootDir + "/constants.js";
+    watchBotFile(bot, constFile, "constants");
+    const event_percentagesFile = rootDir + "/events/event_percentages.js";
+    watchBotFile(bot, event_percentagesFile, "event_percentages");
+
 }
 
 // Reload commands, events, constants, etc. that have had their file changed.
@@ -27,6 +39,42 @@ module.exports.watchBotFiles = (bot) => {
 // as well as a bot.changedFiles map that's used to know what files changed.
 module.exports.reInitBot = (bot) => {
     reinitCmds(bot);
+    reinitUtil(bot);
+
+    const rootDir = __dirname + "/..";
+    bot.constants = reinitFile(bot, rootDir + "/constants.js", "constants");
+    bot.event_percentages = reinitFile(bot, rootDir + "/events/event_percentages.js", "event_percentages");
+}
+
+/*
+function watchBotHelper(bot, path, helperFunc) {
+    fs.watch(path, (event, filename) => {
+        if (filename && event === "change" && filterJsFiles(filename)) {
+            helperFunc(bot, reloadModule(path + filename));
+        }
+    });
+}
+
+function reinitCmd(bot, cmd) {
+
+}*/
+
+// ChangedFilesMap should be a map in the bot.changedFiles object
+function watchBotDir(dirPath, changedFilesMap) {
+    fs.watch(dirPath, (event, filename) => {
+        if (filename && event === "change" && filterJsFiles(filename)) {
+            changedFilesMap.set(dirPath + filename, true);
+        }
+    });
+}
+
+// ChangedFilesProp should be a boolean in the bot.changedFiles object
+function watchBotFile(bot, filePath, changedFilesProp) {
+    fs.watch(filePath, (event, filename) => {
+        if (filename && event === "change" && filename === path.basename(filePath)) {
+            bot.changedFiles[changedFilesProp] = true;
+        }
+    });
 }
 
 function filterJsFiles(file) {
@@ -54,5 +102,41 @@ function reinitCmds(bot) {
             bot.changedFiles.commands.delete(filename);
         });
         bot.printSpace();
+    }
+}
+
+function reinitUtil(bot) {
+    let i = 1;
+    const numUtilChanged = bot.changedFiles.util.size;
+    if (numUtilChanged > 0) {
+        console.log(`Reloading ${numUtilChanged} util files...`);
+        bot.changedFiles.util.forEach((_, filename) => {
+            let bufferFunction = reloadModule(filename);
+            console.log(`${i++}: ${path.basename(filename)} reloaded!`);
+            
+            let functionKeys = Object.keys(bufferFunction);
+            let functionValues = Object.values(bufferFunction);
+        
+            for (i=0; i<functionKeys.length; i++) {
+                bot.util[`${functionKeys[i]}`] = functionValues[i];
+            }
+            bot.changedFiles.util.delete(filename);
+        });
+        bot.printSpace();
+    }
+}
+
+// ChangedFilesProp should be a boolean in the bot.changedFiles object
+function reinitFile(bot, file, changedFilesProp) {
+    if (bot.changedFiles[changedFilesProp]) {
+        console.log(`${path.basename(file)} reloaded!`);
+        bot.printSpace();
+
+        bot.changedFiles[changedFilesProp] = false;
+        // Delete module from cache and require again
+        return reloadModule(file);
+    } else {
+        // Require module from cache
+        return require(file);
     }
 }
