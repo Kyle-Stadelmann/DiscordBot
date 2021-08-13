38b8c29464bc98b2ffefd8b20548ebcf9c011f20
Kyle Stadelmann <dlice@hotmail.com>
2021-08-13 02:16:41 -0700

    added wpm

--- events/lib/message/typing_speed_reporter.js
+++ events/lib/message/typing_speed_reporter.js
@@ -1,10 +1,16 @@
+function countWords(str) {
+    return str.trim().split(/\s+/).length;
+}
+
 module.exports = async (bot, message) => {
     // bot.event_percentages.DANIEL_WPM_CHANCE_FUNCTION(numWords) <-- replace the true with this
     if (true && bot.typingTimestamps.has(message.author.id) && bot.typingTimestamps.get(message.author.id)) {
-        let typingTime = message.createdTimestamp - bot.typingTimestamps.get(message.author.id);
+        // Time spent typing message in seconds
+        let typingTime = (message.createdTimestamp - bot.typingTimestamps.get(message.author.id))/1000;
+        let wpm = countWords(message.content)/typingTime * 60;
 
-        console.log("Reporting a typing time of: " + typingTime/1000 + " for user: " + message.author.id);
-        message.channel.send("That message took approximately " + typingTime/1000 + " seconds");
+        console.log(`Reporting a typing time of: ${typingTime} with WPM of ${wpm} for user: ${message.author.id}`);
+        message.channel.send(`That message took approximately ${typingTime} seconds, with a WPM of ${wpm}`);
         bot.typingTimestamps.set(message.author.id, null);
 
         bot.printSpace();
