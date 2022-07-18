dfb686bae44c9062f7a581c1b8fe67d93106a80f
Kyle Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2022-07-17 18:46:42 -0700

    Fixed cooldowns

--- src/types/containers/cooldown_container.ts
+++ src/types/containers/cooldown_container.ts
@@ -1,15 +1,16 @@
 import { GuildMember } from "discord.js";
 import { Low, JSONFile } from "lowdb";
 import { COOLDOWN_JSON_LOC } from "../../constants.js";
+import { Cooldown, CooldownFile } from "../../util/cooldown_helpers.js";
 
 export class CooldownContainer {
-	private cooldowns: Map<string, Date>;
-	private db: Low;
+	private cooldowns: Cooldown;
+	private db: Low<CooldownFile>;
 
 	constructor(private cooldownTime: number, private cooldownName: string) {
-		const adapter = new JSONFile(COOLDOWN_JSON_LOC);
+		const adapter = new JSONFile<CooldownFile>(COOLDOWN_JSON_LOC);
 		this.db = new Low(adapter);
-		this.cooldowns = new Map<string, Date>();
+		this.cooldowns = {};
 
 		// Sync execution of async function, but won't cause realistic issues here for the current scope of this bot
 		// eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -18,26 +19,25 @@ export class CooldownContainer {
 
 	public isOnCooldown(member: GuildMember): boolean {
 		const memberId = member.id;
+		const cooldownDate = this.cooldowns[`${memberId}`];
 
-		if (!this.cooldowns.has(memberId)) return false;
+		if (!cooldownDate) return false;
 
-		const endCooldownTime = this.cooldowns.get(memberId);
-		if (endCooldownTime > new Date()) return true;
+		if (cooldownDate > new Date()) return true;
 		return false;
 	}
 
 	public async putOnCooldown(member: GuildMember) {
 		const endCooldownEpoch = new Date().valueOf() + this.cooldownTime;
 		const endCooldownDate = new Date(endCooldownEpoch);
-
-		this.cooldowns.set(member.id, endCooldownDate);
-
+		
+		this.cooldowns[`${member.id}`] = endCooldownDate;
 		// TODO: Maybe remove the await on this (doubt it will slow command handling realistically though)
 		await this.updateDb();
 	}
 
 	public async endCooldown(member: GuildMember) {
-		this.cooldowns.delete(member.id);
+		delete this.cooldowns[`${member.id}`]
 
 		// TODO: Maybe remove the await on this (doubt it will slow command handling realistically though)
 		await this.updateDb();
@@ -46,18 +46,14 @@ export class CooldownContainer {
 	private async updateDb() {
 		await this.db.read();
 
-		// Produces { commandName: cooldowns }
-		this.db.data[this.cooldownName] = this.cooldowns;
-
+		this.db.data[`${this.cooldownName}`] = this.cooldowns;
+		
 		await this.db.write();
 	}
 
 	private async initCooldowns() {
 		await this.db.read();
-		if (this.db.data && this.db.data[`${this.cooldownName}`]) {
-			this.cooldowns = this.db.data[`${this.cooldownName}`] as Map<string, Date>;
-		} else {
-			this.cooldowns = new Map<string, Date>();
-		}
+		const existingCd = this.db.data[`${this.cooldownName}`]
+		this.cooldowns = existingCd ?? {};
 	}
 }
