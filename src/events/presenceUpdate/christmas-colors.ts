613c79e400228695e2b7ebacb7f16e7499995e72
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2022-12-01 18:42:41 -0800

    Create HolidayColorCycler and added christmas color cycling

--- src/events/presenceUpdate/christmas-colors.ts
+++ src/events/presenceUpdate/christmas-colors.ts
@@ -1,19 +1,19 @@
 import { ArgsOf, Discord, On } from "discordx";
-import { HOLIDAY_BLACK_ROLE_ID, HOLIDAY_ORANGE_ROLE_ID, HOLIDAY_WHITE_ROLE_ID } from "../../constants.js";
+import { HOLIDAY_GREEN_ROLE_ID, HOLIDAY_RED_ROLE_ID, HOLIDAY_WHITE_ROLE_ID } from "../../constants.js";
 import { HolidayColorCycler, Month } from "../../types/holiday-color-cycler.js";
 
-const halloweenRoleIds = [
-	HOLIDAY_ORANGE_ROLE_ID,
+const christmasRoleIds = [
 	HOLIDAY_WHITE_ROLE_ID,
-	HOLIDAY_BLACK_ROLE_ID
+	HOLIDAY_GREEN_ROLE_ID,
+	HOLIDAY_RED_ROLE_ID
 ];
 
 @Discord()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-abstract class HalloweenColors {
+abstract class ChristmasColors {
     @On({event: "presenceUpdate"})
     private async updateHolidayColors([oldPresence, newPresence]: ArgsOf<"presenceUpdate">) {
-		const holidayColorCycler = new HolidayColorCycler(halloweenRoleIds, Month.October);
+		const holidayColorCycler = new HolidayColorCycler(christmasRoleIds, Month.December);
 		await holidayColorCycler.tryHolidayColors(oldPresence, newPresence);
 	}
 }
