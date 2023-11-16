5ac4b78fb13965f9ea003a8fa5d2531794188495
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2023-11-15 19:05:28 -0800

    Ran formatter

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,7 +3,7 @@ export * from "./db-helper.js";
 export * from "./delete-voice-channel.js";
 export * from "./embed-helpers.js";
 export * from "./enum-helper.js";
-export * from "./error-helper.js"
+export * from "./error-helper.js";
 export * from "./general.js";
 export * from "./idea-helpers.js";
 export * from "./music-helpers.js";
