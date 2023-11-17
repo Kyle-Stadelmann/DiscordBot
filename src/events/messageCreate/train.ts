5a4f82b266c206fbb338578d308e68a71431a19f
Justin Mulligan <jtmmulligan@gmail.com>
2023-11-17 10:30:29 -0800

    FIX: eslint nags + formatting

--- src/events/messageCreate/train.ts
+++ src/events/messageCreate/train.ts
@@ -1,35 +1,34 @@
 import { ArgsOf, Discord, On } from "discordx";
 
-var activeTrain = false;
-var previousString = '';
-var previousUser = '';
+let activeTrain = false;
+let previousString = "";
+let previousUser = "";
 @Discord()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 abstract class Train {
 	@On({ event: "messageCreate" })
 	private async handleTrain([msg]: ArgsOf<"messageCreate">) {
-    if(!activeTrain){
-      msg.channel.messages.fetch({limit: 2}).then (async msgs => {
-        for await (let element of msgs){
-          if(previousString === element[1].content && previousUser !== element[1].author.id){
-            // last 2 messages were the same and written by different people, train time bby
-            activeTrain = true
-            await msg.channel.send(previousString)
-            break;
-          } else {
-            // buisness as usual...
-            previousString = element[1].content
-            previousUser = element[1].author.id
-          }
-        }
-      })
-    } else {
-      // wait to end activeTrain
-      if(previousString !== msg.content){
-        activeTrain = false
-      } else {
-        previousString = msg.content
-      }
-    }
-  }
+		if (!activeTrain) {
+			// eslint-disable-next-line no-void
+			void msg.channel.messages.fetch({ limit: 2 }).then(async (msgs) => {
+				for await (const element of msgs) {
+					if (previousString === element[1].content && previousUser !== element[1].author.id) {
+						// last 2 messages were the same and written by different people, train time bby
+						activeTrain = true;
+						await msg.channel.send(previousString);
+						break;
+					} else {
+						// buisness as usual...
+						previousString = element[1].content;
+						previousUser = element[1].author.id;
+					}
+				}
+			});
+		} else if (previousString !== msg.content) {
+			// wait to end activeTrain
+			activeTrain = false;
+		} else {
+			previousString = msg.content;
+		}
+	}
 }
