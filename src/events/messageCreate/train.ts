d0d67cb75bd92dcede2605eb53bc73052ac013a8
Justin Mulligan <jtmmulligan@gmail.com>
2023-11-17 16:46:47 -0800

    refactor: removed dumbness
    current bug is msgs is undefined sometimes, not sure why since await is there

--- src/events/messageCreate/train.ts
+++ src/events/messageCreate/train.ts
@@ -1,34 +1,25 @@
 import { ArgsOf, Discord, On } from "discordx";
 
 let activeTrain = false;
-let previousString = "";
-let previousUser = "";
 @Discord()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 abstract class Train {
 	@On({ event: "messageCreate" })
 	private async handleTrain([msg]: ArgsOf<"messageCreate">) {
 		if (!activeTrain) {
-			// eslint-disable-next-line no-void
-			void msg.channel.messages.fetch({ limit: 2 }).then(async (msgs) => {
-				for await (const element of msgs) {
-					if (previousString === element[1].content && previousUser !== element[1].author.id) {
-						// last 2 messages were the same and written by different people, train time bby
-						activeTrain = true;
-						await msg.channel.send(previousString);
-						break;
-					} else {
-						// buisness as usual...
-						previousString = element[1].content;
-						previousUser = element[1].author.id;
-					}
-				}
-			});
-		} else if (previousString !== msg.content) {
-			// wait to end activeTrain
-			activeTrain = false;
-		} else {
-			previousString = msg.content;
+			const msgs = await msg.channel.messages.fetch({ limit: 2 });
+			console.log(`msgs:${msgs}`);
+			console.log("++++++++++++++++++++++++++++++++++++++++++++++++++");
+			console.log("++++++++++++++++++++++++++++++++++++++++++++++++++");
+			console.log("++++++++++++++++++++++++++++++++++++++++++++++++++");
+			console.log(`msgs[0]:${msgs[0]}`);
+			if (msgs[0].content === msgs[1].content && msgs[0].user.id !== msgs[1].user.id) {
+				// last 2 messages were the same and written by different people, train time bby
+				activeTrain = true;
+				await msg.channel.send(msgs[0].content);
+			} else if (msgs[0].content !== msgs[1].content) {
+				activeTrain = false;
+			}
 		}
 	}
 }
