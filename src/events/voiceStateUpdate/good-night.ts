be8628218e7af1fc5a9843596529ff59427a83d2
Justin Mulligan <jtmmulligan@gmail.com>
2023-11-20 16:01:18 -0800

    switch prod variables, tenor api errors visible

--- src/events/voiceStateUpdate/good-night.ts
+++ src/events/voiceStateUpdate/good-night.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { EmbedBuilder, GuildMember, TextBasedChannel } from "discord.js";
 import { ArgsOf, Discord, On } from "discordx";
 import { client } from "../../app.js";
-import { BD5_BOT_STUFF_CHANNEL_ID, BD5_DEV_SERVER_IDS, DEV_SERVER_TESTING_CHANNEL_1_ID } from "../../constants.js";
-import { getRandomElement, hasHumans, isNullOrUndefined, random } from "../../util/index.js";
+import { BD5_BOT_STUFF_CHANNEL_ID, BD5_DEV_SERVER_IDS } from "../../constants.js";
+import { getRandomElement, hasHumans, isNullOrUndefined, random, sendErrorToDiscordChannel } from "../../util/index.js";
 
 const GOOD_NIGHT_VARIATIONS = [
 	"goot!",
@@ -20,36 +20,6 @@ const KISS_CHANCE = 10;
 
 const leftOnLog = new Map<GuildMember, Date>();
 
-/*
-  on a voice status update{
-
-    if (user disconnects){
-
-      const leftOnLog = map of user to the time of disconnect
-
-      if (between 22:00-06:00 && channel has no humans){
-
-        const last15Mins = users who have disconnected within the last 15 minutes (calculated from leftOnLog)
-        let goodNightMsg = ''
-
-        last15Mins.forEach(user => {
-          goodNightMsg += `${user.toString()} `
-        })
-
-        goodNightMsg += '\n'
-        goodNightMsg += randomArrayElement(goodNightVariations)
-
-        if(kissChance){
-          goodNightMsg += 'good night anime kiss' tenor query
-        } else {
-          goodNightMsg += 'good night anime
-        }
-        await #bot-stuff.send(goodNightMsg)
-      }
-    }
-  }
-*/
-
 async function randomGifUrl(lmt, searchString): Promise<string> {
 	const searchUrl = `https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&q=${searchString}&limit=${lmt}&random=true`;
 	const { url } = (await axios.get(searchUrl)).data.results[0].media_formats.gif;
@@ -65,8 +35,7 @@ abstract class GoodNight {
 		// TODO: We need a guild config that allows us to use bot_stuff channels more generally
 		if (!BD5_DEV_SERVER_IDS.includes(newState.guild.id)) return;
 
-		// const botStuffChannel = client.channels.resolve(BD5_BOT_STUFF_CHANNEL_ID) as TextBasedChannel;
-		const test1Channel = client.channels.resolve(DEV_SERVER_TESTING_CHANNEL_1_ID) as TextBasedChannel;
+		const botStuffChannel = client.channels.resolve(BD5_BOT_STUFF_CHANNEL_ID) as TextBasedChannel;
 
 		if (isNullOrUndefined(newState.channelId)) {
 			const currTime = new Date();
@@ -96,10 +65,9 @@ abstract class GoodNight {
 
 					embed.setImage(gifUrl);
 
-					// await botStuffChannel.send({ embeds: [embed] });
-					await test1Channel.send({ embeds: [embed] });
+					await botStuffChannel.send({ embeds: [embed] });
 				} catch (error) {
-					console.error(error);
+					await sendErrorToDiscordChannel(error);
 				}
 			}
 		}
