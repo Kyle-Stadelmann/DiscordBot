3a81f38775a5b3126263c5fd4c2bb6566823fe20
Kyle-Stadelmann <kyle.stadelmann@gmail.com>
2024-10-07 16:24:24 -0700

    Train event happens after X msgs (now 4)

--- src/events/messageCreate/train.ts
+++ src/events/messageCreate/train.ts
@@ -2,22 +2,27 @@ import { ArgsOf, Discord, On } from "discordx";
 import { client } from "../../app.js";
 
 const activeTrains = new Set<String>();
+
+const START_TRAIN_COUNT = 4;
+
 @Discord()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 abstract class Train {
 	@On({ event: "messageCreate" })
 	private async handleTrain([msg]: ArgsOf<"messageCreate">) {
-		const msgs = await msg.channel.messages.fetch({ limit: 2 });
+		const msgs = await msg.channel.messages.fetch({ limit: START_TRAIN_COUNT });
+
 		if (msgs.every((e) => e.content === "")) return;
-		if (
-			!activeTrains.has(msg.channelId) &&
-			msgs.at(0).content === msgs.at(1).content &&
-			msgs.at(0).author.id !== msgs.at(1).author.id
-		) {
+		// Checks if train started/continued off of a bot message
+		if (msgs.some((e) => e.author.id === client.user.id)) return;
+
+		const msgContents = new Set(msgs.map((m) => m.content));
+		const msgAuthors = new Set(msgs.map((m) => m.author.id));
+
+		if (!activeTrains.has(msg.channelId) && msgContents.size === 1 && msgAuthors.size === START_TRAIN_COUNT) {
 			activeTrains.add(msg.channelId);
-			if (msgs.some((e) => e.author.id === client.user.id)) return;
 			await msg.channel.send(msgs.at(0).content);
-		} else if (msgs.at(0).content !== msgs.at(1).content) {
+		} else if (msgContents.size > 1) {
 			activeTrains.delete(msg.channelId);
 		}
 	}
