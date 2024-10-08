e432ea5a2372de7f933c3e377cb2e9c9f62e4174
Kyle-Stadelmann <kyle.stadelmann@gmail.com>
2024-10-07 19:14:53 -0700

    Simplified zacc event, better matches casing of original message

--- src/events/messageCreate/zach-zacc.ts
+++ src/events/messageCreate/zach-zacc.ts
@@ -1,28 +1,19 @@
 import { ArgsOf, Discord, On } from "discordx";
 import { random } from "../../util/index.js";
 
-const ZACC_CHANCE = 5;
+const ZACC_CHANCE = 7.5;
+const ZACH_REGEX = /[Zz][Aa][Cc][Hh]/gm;
 
 @Discord()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 abstract class ZachZacc {
 	@On({ event: "messageCreate" })
 	private async handleZachZacc([msg]: ArgsOf<"messageCreate">) {
-		const content = msg.content.toLowerCase();
-		// Only continue if "zach" is found in a message
-		if (!content.includes("zach")) return;
+		const { content } = msg;
+		if (msg.author.bot || !content.match(ZACH_REGEX)) return;
 
 		if (random(ZACC_CHANCE)) {
-			let output = "";
-			// loop through until message has no more "zach"'s
-			while (content.includes("zach")) {
-				const zachIndex = content.indexOf("zach");
-				const firstHalf = content.substring(0, zachIndex);
-				const lastHalf = content.substring(zachIndex + 4);
-
-				output += `${firstHalf}zacc${lastHalf}`;
-			}
-
+			const output = content.replaceAll(ZACH_REGEX, (s) => s.slice(0, 3) + s.charAt(2));
 			await msg.channel.send(`${output}*`);
 		}
 	}
